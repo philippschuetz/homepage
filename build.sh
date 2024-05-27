696f3c5800db3bac1c37cc238065e7c97a5d089a
philipp-schuetz <mail@philippschuetz.com>
2024-05-27 15:58:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+
+# clear contents of the output directory
 rm -rf out/*
 
-placeholder_content="<!--content-->"
+PLACEHOLDER_CONTENT="<!--content-->"
+PLACEHOLDER_TEMPLATES_PREFIX="<!--templates-"
+PLACEHOLDER_TEMPLATES_SUFFIX="-->"
+SOURCE_DIR="src/content"
+DESTINATION_DIR="out"
 
-# generate files in the out directory
-content="test content"
-sed "s/${placeholder_content}/${content}/" index.html
+# define replace values
+REPLACE_KEYS=("testkey1" "testkey2")
+REPLACE_VALUES=("testvalue1" "testvalue2")
+
+# Iterate through each file in the source directory
+find "$SOURCE_DIR" -type f | while read -r file; do
+
+    # relative path of the file from the source directory
+    relative_path="${FILE#$SOURCE_DIR/}"
+
+    # create the destination directory if it doesn't exist
+    mkdir -p "$DESTINATION_DIR/$(dirname "$relative_path")"
+
+    # search & replace content, copy the content to the destination directory
+    file=$(echo $file | sed -e "s/${PLACEHOLDER_CONTENT}/${content}/")
+    cp "$file" "$DESTINATION_DIR/$relative_path"
+done
