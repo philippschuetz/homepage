2f369d006c583028e5ca61197422f6392f0074f9
philipp-schuetz <mail@philippschuetz.com>
2024-05-28 19:19:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ rm -rf out/*
 PLACEHOLDER_CONTENT="<!--content-->"
 PLACEHOLDER_TEMPLATES_PREFIX="<!--templates-"
 PLACEHOLDER_TEMPLATES_SUFFIX="-->"
+HTML_TEMPLATE=$(cat "src/templates/index.html")
 SOURCE_DIR="src/content"
 DESTINATION_DIR="out"
 
@@ -13,16 +14,18 @@ DESTINATION_DIR="out"
 REPLACE_KEYS=("testkey1" "testkey2")
 REPLACE_VALUES=("testvalue1" "testvalue2")
 
+
+
 # Iterate through each file in the source directory
 find "$SOURCE_DIR" -type f | while read -r file; do
 
     # relative path of the file from the source directory
-    relative_path="${FILE#$SOURCE_DIR/}"
+    relative_path="${file#$SOURCE_DIR/}"
 
     # create the destination directory if it doesn't exist
     mkdir -p "$DESTINATION_DIR/$(dirname "$relative_path")"
 
     # search & replace content, copy the content to the destination directory
-    file=$(echo $file | sed -e "s/${PLACEHOLDER_CONTENT}/${content}/")
-    cp "$file" "$DESTINATION_DIR/$relative_path"
+    new_content=$(echo $HTML_TEMPLATE | sed "s|${PLACEHOLDER_CONTENT}|$(cat ${file})|g")
+    echo $new_content > "$DESTINATION_DIR/$relative_path"
 done
