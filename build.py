1825ece60d6d5e6dad796b467db7d9433eab8f83
philipp-schuetz <mail@philippschuetz.com>
2024-05-30 19:56:40 +0200

    Rename templates to components

--- build.py
+++ build.py
@@ -1,15 +1,13 @@
-import os
-import glob
 import shutil
 import datetime
 from pathlib import Path
 
 PLACEHOLDER_CONTENT = "<!--content-->"
-PLACEHOLDER_TEMPLATES_PREFIX = "<!--templates-"
+PLACEHOLDER_TEMPLATES_PREFIX = "<!--components-"
 PLACEHOLDER_TEMPLATES_SUFFIX = "-->"
 HTML_TEMPLATE = open("src/base.html").read()
 CONTENT_DIR = Path("src/content")
-TEMPLATES_DIR = Path("src/templates")
+TEMPLATES_DIR = Path("src/components")
 DESTINATION_DIR = Path("out")
 REPLACE_DICT = {
     "<!--year-->": str(datetime.datetime.now().year)
@@ -32,7 +30,7 @@ for file in Path(CONTENT_DIR).rglob('*'):
     for key, value in REPLACE_DICT.items():
         new_content = new_content.replace(key, value)
 
-    # add content from templates files
+    # add content from components files
     for tmp_file in TEMPLATES_DIR.rglob('*'):
         if file.is_file():
             old = f"{PLACEHOLDER_TEMPLATES_PREFIX}{tmp_file.stem}{PLACEHOLDER_TEMPLATES_SUFFIX}"
