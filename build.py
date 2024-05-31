61fd0d5a5f37c38b58e124ee9ec874e20e9b477f
philipp-schuetz <mail@philippschuetz.com>
2024-05-31 18:28:49 +0200

    Fix issue with build.py where replacing was performed on non-html files

--- build.py
+++ build.py
@@ -25,18 +25,20 @@ def main():
             continue
         relative_path = file.relative_to(CONTENT_DIR)
         Path(DESTINATION_DIR, relative_path.parent).mkdir(parents=True, exist_ok=True)
-        # search & replace html content
-        new_content = HTML_TEMPLATE.replace(PLACEHOLDER_CONTENT, open(file).read())
+        new_content = open(file).read()
+        if file.suffix == '.html':
+            # search & replace html content
+            new_content = HTML_TEMPLATE.replace(PLACEHOLDER_CONTENT, open(file).read())
 
-        # search and replace with REPLACE_KEYS and REPLACE_VALUES
-        for key, value in REPLACE_DICT.items():
-            new_content = new_content.replace(key, value)
+            # search and replace with REPLACE_KEYS and REPLACE_VALUES
+            for key, value in REPLACE_DICT.items():
+                new_content = new_content.replace(key, value)
 
-        # add content from components files
-        for tmp_file in TEMPLATES_DIR.rglob('*'):
-            if file.is_file():
-                old = f'{PLACEHOLDER_TEMPLATES_PREFIX}{tmp_file.stem}{PLACEHOLDER_TEMPLATES_SUFFIX}'
-                new_content = new_content.replace(old, open(tmp_file).read())
+            # add content from components files
+            for tmp_file in TEMPLATES_DIR.rglob('*'):
+                if file.is_file():
+                    old = f'{PLACEHOLDER_TEMPLATES_PREFIX}{tmp_file.stem}{PLACEHOLDER_TEMPLATES_SUFFIX}'
+                    new_content = new_content.replace(old, open(tmp_file).read())
 
         # write content to file
         with open(Path(DESTINATION_DIR, relative_path), 'w') as f:
